34956f7ef8ec46c6da90d12c6a9637e1a6af0e79
WTT <wilsonteng89@gmail.com>
2020-11-20 13:10:13 +0800

    Hello world example
    https://struts.apache.org/getting-started/hello-world-using-struts2.html

---
+++ webapp-login/src/main/java/cz3002/grp14/controller/HelloWorldAction.java
@@ -0,0 +1,21 @@
+package cz3002.grp14.controller;
+
+import cz3002.grp14.model.MessageStore;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+
+    /** Properties **/
+    private MessageStore messageStore;
+
+    public String execute() {
+        messageStore = new MessageStore() ;
+
+        return SUCCESS;
+    }
+
+    public MessageStore getMessageStore() {
+        return messageStore;
+    }
+}
---
+++ webapp-login/src/main/java/cz3002/grp14/model/MessageStore.java
@@ -0,0 +1,13 @@
+package cz3002.grp14.model;
+
+public class MessageStore {
+    private String message;
+
+    public MessageStore() {
+        message = "Hello Struts User";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
